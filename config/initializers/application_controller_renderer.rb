f86620fd2802fc6021b931235ca0239c14e6a357
mizu11pro <mizuakasa@gmail.com>
2020-11-15 14:59:31 +0000

    [1-1]

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'ed537c28275d2e1503ecf0dda1078ba4cbf71454d26347517c37db5c67ecc3cdfa1f99843c42e64c60b2d21fbbc34e9dd9fe6d5d81563b383070f5b34c6c9e89'
